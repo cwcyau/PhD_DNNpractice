3a542e3c3ab6ae357a0cd53efe86b28e8bd6278e
NelsonZhouwenhua <33813672+NelsonZhouwenhua@users.noreply.github.com>
2020-10-01 22:51:37 +0100

    Simple DNN model of logistic regression

--- code/week1.py
+++ code/week1.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.autograd import Variable
 
 torch.manual_seed(1)
 
@@ -31,6 +32,7 @@ train_x = x[0:100,:]
 train_y = y[0:100]
 test_x = x[100:200,:]
 test_y = y[100:200]
+
 
 #############################################
 # run standard logistic regression on simulated data
@@ -67,22 +69,28 @@ class Logistictest(nn.Module):
 model = Logistictest()
 
 # define a loss function and optimizer
-criterion = torch.nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001)
 
 # make dataset iterable
-train_loader_x = torch.utils.data.DataLoader(dataset=train_x, batch_size=4, shuffle=True)
-train_loader_y = torch.utils.data.DataLoader(dataset=train_y, batch_size=4, shuffle=True)
-test_loader_x = torch.utils.data.DataLoader(dataset=test_x, batch_size=4, shuffle=False)
-test_loader_y = torch.utils.data.DataLoader(dataset=test_y, batch_size=4, shuffle=False)
+train_loader_x = Variable(torch.Tensor(train_x))
+train_loader_y = Variable(torch.Tensor(train_y))
+test_loader_x = Variable(torch.Tensor(test_x))
+test_loader_y = Variable(torch.Tensor(test_y))
+# train_loader_x = torch.utils.data.DataLoader(dataset=train_x, batch_size=4, shuffle=True)
+# train_loader_y = torch.utils.data.DataLoader(dataset=train_y, batch_size=4, shuffle=True)
+# test_loader_x = torch.utils.data.DataLoader(dataset=test_x, batch_size=4, shuffle=False)
+# test_loader_y = torch.utils.data.DataLoader(dataset=test_y, batch_size=4, shuffle=False)
 
 # model training
 iter = 0
 for epoch in range(5):  # loop over the dataset multiple times
 
+    running_loss = 0.0
     for i, data in enumerate(zip(train_loader_x,train_loader_y)):
         inputs, labels = data
-        inputs = inputs.type(torch.LongTensor)
+        # reshape train data y
+        labels = labels.view(1)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -95,22 +103,139 @@ for epoch in range(5):  # loop over the dataset multiple times
         # optimize
         optimizer.step()
 
-        iter+=1
-        if iter%500==0:
-            # calculate Accuracy
-            correct = 0
-            total = 0
-            for inputs, labels in zip(test_loader_x,test_loader_y):
-                outputs = model(inputs)
-                _, predicted = torch.max(outputs.data, 1)
-                total+= labels.size(0)
-                # for gpu, bring the predicted and labels back to cpu fro python operations to work
-                correct+= (predicted == labels).sum()
-            accuracy = 100 * correct/total
-            print("Iteration: {}. Loss: {}. Accuracy: {}.".format(iter, loss.item(), accuracy))
+        # print statistics
+        running_loss += loss.item()
+        if i % 20 == 19:  # print every 20 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
 
+print('Finished Training')
+
+# make predictions
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in zip(train_loader_x,train_loader_y):
+        inputs, labels = data
+        # reshape test data y
+        labels = labels.view(1)
+
+        outputs = model(inputs)
+
+        predicted = torch.round(outputs)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network 100 train numbers: %d %%' % (
+    100 * correct / total))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in zip(test_loader_x,test_loader_y):
+        inputs, labels = data
+        # reshape test data y
+        labels = labels.view(1)
+
+        outputs = model(inputs)
+
+        predicted = torch.round(outputs)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network 100 test numbers: %d %%' % (
+    100 * correct / total))
 
 
 #############################################
 # apply it on a multiple layers DNN
 #############################################
+
+# use multiple layers NN
+class LogisticDNN(nn.Module):
+
+    def __init__(self):
+        super(LogisticDNN, self).__init__()
+        self.linear1 = nn.Linear(5, 60)
+        self.linear2 = nn.Linear(60, 20)
+        self.linear3 = nn.Linear(20, 10)
+        self.linear4 = nn.Linear(10, 5)
+        self.linear5 = nn.Linear(5, 1)
+
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.linear2(x)
+        x = self.linear3(x)
+        x = self.linear4(x)
+        x = self.linear5(x)
+        return x
+
+model = LogisticDNN()
+
+# define a loss function and optimizer
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+
+# model training
+iter = 0
+for epoch in range(5):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(zip(train_loader_x,train_loader_y)):
+        inputs, labels = data
+        # reshape train data y
+        labels = labels.view(1)
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        # Forward pass
+        outputs = model(inputs)
+        # Compute Loss
+        loss = criterion(outputs, labels)
+        # Backward pass
+        loss.backward()
+        # optimize
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 20 == 19:  # print every 20 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# make predictions
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in zip(train_loader_x,train_loader_y):
+        inputs, labels = data
+        # reshape test data y
+        labels = labels.view(1)
+
+        outputs = model(inputs)
+
+        predicted = torch.round(outputs)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network 100 train numbers: %d %%' % (
+    100 * correct / total))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in zip(test_loader_x,test_loader_y):
+        inputs, labels = data
+        # reshape test data y
+        labels = labels.view(1)
+
+        outputs = model(inputs)
+
+        predicted = torch.round(outputs)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy of the network 100 test numbers: %d %%' % (
+    100 * correct / total))
